cf5d6880d5d89b8206ecbebdf20397cec65d8908
dfsalam <dfsalam@gmail.com>
2023-06-23 21:35:24 -0500

    Create back-end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+m1 = 'Do you remember where you were six months ago?'
+m2 = 'Keep up the effort, imagine what you will know in six months...'
+m3 = 'Hello web developer.'
+m4 = 'Good morning! Embrace the new day with a positive mindset and the determination to achieve greatness.'
+m5 = 'Hello! Remember that every day is a fresh start, filled with opportunities and possibilities. Make the most of it and believe in your ability to succeed.'
+
+g1 = Greeting.create!(message:m1)
+g2 = Greeting.create!(message:m2)
+g3 = Greeting.create!(message:m3)
+g4 = Greeting.create!(message:m4)
+g5 = Greeting.create!(message:m5)
